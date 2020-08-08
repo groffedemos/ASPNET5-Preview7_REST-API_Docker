4da0a72dfbb16ebbb8b2a45f8ae1519968940be2
Renato Groffe <renato.groffe@yahoo.com.br>
2020-08-08 11:44:25 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -33,7 +33,7 @@ namespace APIContagem.Controllers
                     _CONTADOR.Local,
                     _CONTADOR.Kernel,
                     _CONTADOR.TargetFramework,
-                    MensagemFixa = "Teste",
+                    MensagemFixa = "Primeiro exemplo com Docker no Treinamento",
                     MensagemVariavel = _configuration["MensagemVariavel"]
                 };
             }
